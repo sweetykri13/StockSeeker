26c5628efba9954528fb5ea6d10ccfffe44ebb00
Sweety <sweetykri17@gmail.com>
2024-04-06 11:18:47 +0530

    feat: home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './Pages/navbar/navbar.component';
+import { HomeComponent } from './Pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'navbar',component:NavbarComponent},
+  {path:'home',component:HomeComponent},
+  {path:'',component:HomeComponent},
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
